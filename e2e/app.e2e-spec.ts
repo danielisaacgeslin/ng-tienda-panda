11353edcc384072e666b3c35d1e4448c96296961
danielisaacgeslin <danielisaacgeslin@gmail.com>
2017-09-03 21:11:37 +0200

    started with e2e testing

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -7,8 +7,9 @@ describe('ng-tienda-panda App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  it('should contain alt on the main logo', next => {
+    page.navigateTo('/');
+    expect(page.getMainLogoAlt()).toEqual('tienda panda');
+    next();
   });
 });
