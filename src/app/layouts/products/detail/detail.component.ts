4e2b85a61c4a2a26766327411514688511068e58
danielisaacgeslin <danielisaacgeslin@gmail.com>
2017-09-08 21:20:31 +0200

    changes

--- src/app/layouts/products/detail/detail.component.ts
+++ src/app/layouts/products/detail/detail.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { DOCUMENT } from '@angular/platform-browser';
 
 import { MLService } from '../../../services';
 
@@ -11,17 +12,21 @@ import { MLService } from '../../../services';
 export class DetailComponent implements OnInit {
 
   public article: IArticle = {};
+  
 
-  constructor(private route: ActivatedRoute, private MLService: MLService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private MLService: MLService,
+    @Inject(DOCUMENT) private document: Document
+    ) { }
 
   public ngOnInit() {
     this.route.params.take(1)
       .map(params => params.id)
       .switchMap(id => this.MLService.getItem(id))
       .do(article => this.article = article)
-      .do(a=>console.log(a))
       .subscribe();
-
+    this.document.body.scrollTop = 0;
   }
 
 }
