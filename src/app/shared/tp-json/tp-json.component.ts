2e360801862ae607f61df4b20c9f75cffb23da21
danielisaacgeslin <danielisaacgeslin@gmail.com>
2017-09-15 17:18:21 +0200

    upgrades

--- src/app/shared/tp-json/tp-json.component.ts
+++ src/app/shared/tp-json/tp-json.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Inject } from '@angular/core';
+import { DOCUMENT } from "@angular/platform-browser";
 
 @Component({
   selector: 'tp-json',
@@ -7,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TpJSONComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('content') content: ElementRef;
+  private dom: Document;
+
+  constructor( @Inject(DOCUMENT) dom: Document) {
+    this.dom = dom;
+  }
 
   public ngOnInit() {
 
+  }
+
+  public copy(): void {
+    const text: string = this.content.nativeElement.innerHTML;
+    const elem = this.dom.createElement("textarea");
+    elem.value = text;
+    this.dom.body.appendChild(elem);
+    elem.select()
+    this.dom.execCommand('copy');
+    elem.remove();
   }
 
 }
