59257c82b865f7e94d57370f993620e3e4f24a60
danielisaacgeslin <danielisaacgeslin@gmail.com>
2017-09-15 16:50:33 +0200

    upgrades

--- src/app/layouts/admin-tools/admin-tools-list/admin-tools-list.component.ts
+++ src/app/layouts/admin-tools/admin-tools-list/admin-tools-list.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, AbstractControl } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  AbstractControl,
+  Validators,
+  ValidatorFn,
+  ValidationErrors
+} from '@angular/forms';
 
 import { MLService, HttpHelperService } from '../../../services';
 
@@ -34,15 +41,13 @@ export class AdminToolsListComponent implements OnInit {
   }
 
   public onSubmit(): void {
+    if (!this.formGroup.valid) return;
+
     const MLIds: any = {};
     for (let controlKey in this.formGroup.controls) {
       let key: string = controlKey.slice(0, controlKey.indexOf('-'));
       MLIds[key] = MLIds[key] || [];
-      let value = this.formGroup.controls[controlKey].value || '';
-      if(!value || value.indexOf(this.idPrefix) === -1) continue;
-      while (value.indexOf('-') !== -1) value = value.replace('-', '');
-      let index: number = value.indexOf(this.idPrefix);
-      value = value.slice(index, index + this.idLength);
+      const value = this.generateId(this.formGroup.controls[controlKey].value);
       if (value) MLIds[key].push(value);
     }
     this.MLIds = MLIds;
@@ -53,14 +58,65 @@ export class AdminToolsListComponent implements OnInit {
     this.hiddenCategory[key] = !this.hiddenCategory[key];
   }
 
+  public removeInput(key: string): void {
+    this.formGroup.removeControl(key);
+  }
+
+  public addInputOfType(key: string): void {
+    const keyData = this.getKeyData(key);
+    this.formGroup.addControl(`${keyData.type}-${<any>keyData.number + 1}`, this.generateFormControl(''));
+  }
+
+  public isLastOfType(keyToCheck: string): boolean {
+    const keyToCheckData = this.getKeyData(keyToCheck);
+    const keys: string[] = Object.keys(this.formGroup.controls);
+    let maxNumber: number = keys
+      .filter(key => key.split('-')[0] === keyToCheckData.type)
+      .map(key => Number(key.split('-')[1]))
+      .reduce((prev, current) => prev > current ? prev : current, 0);
+
+    return keyToCheckData.number === maxNumber;
+  }
+
+  public isLastRemainingOfGroup(keyToCheck: string): boolean {
+    const keyToCheckData = this.getKeyData(keyToCheck);
+    const keys: string[] = Object.keys(this.formGroup.controls);
+    return keys.filter(key => key.split('-')[0] === keyToCheckData.type).length < 2;
+  }
+
+  private getKeyData(key: string): { type: string; number: Number } {
+    return { type: key.split('-')[0], number: Number(key.split('-')[1]) };
+  }
+
+  private generateId(input: string): string {
+    let value = input || '';
+
+    while (value.indexOf('-') !== -1) value = value.replace('-', '');
+    const prefixIndex: number = value.indexOf(this.idPrefix);
+    value = value.slice(prefixIndex, prefixIndex + this.idLength);
+    const isValid: boolean =
+      value.indexOf(this.idPrefix) !== -1 &&
+      !isNaN(<any>value.slice(this.idPrefix.length, value.length)) &&
+      value.length === this.idLength;
+    return isValid && value;
+  }
+
   private resetForm(): void {
     let controls: { [key: string]: AbstractControl } = {};
     for (let key in this.MLIds) {
       this.MLIds[key].forEach((id: string, index: number) => {
-        controls[`${key}-${index}`] = new FormControl(id);
+        controls[`${key}-${index}`] = this.generateFormControl(id);
       });
     }
     this.formGroup = new FormGroup(controls);
+  }
+
+  private validate(): ValidatorFn {
+    return (control: AbstractControl) => this.generateId(control.value) ? null : { error: true };
+  }
+
+  private generateFormControl(value: string): AbstractControl {
+    return new FormControl(value, [Validators.required, this.validate()]);
   }
 
 }
