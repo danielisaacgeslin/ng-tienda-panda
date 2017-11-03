bad397dee39ce430890ea1db6a9d021b99aaa482
danielisaacgeslin <danielisaacgeslin@gmail.com>
2017-11-03 19:29:40 +0100

    nav name changes

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -8,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   public readonly routes: { link: string; name: string }[] = [
-    { link: '/productos', name: 'Productos' },
-    { link: '/promociones', name: 'Promociones' },
-    { link: '/segunda-mano', name: 'Segunda Mano' }
+    { link: '/productos', name: 'Hogas' },
+    { link: '/promociones', name: 'Bijou' },
+    { link: '/segunda-mano', name: 'Varios' }
   ];
 
   constructor() { }
